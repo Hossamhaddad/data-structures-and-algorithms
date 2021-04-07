016c0db287edb1071d2ed62b46cb2e82ffcf82d4
hossamhaddad <husamhaddad80@gmail.com>
2021-04-07 04:05:59 +0300

    code challenge3

---
+++ challenges/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch{
+  public static void main(String[] args)
+  {
+   int [] sort={2,3,4,5,6,7};
+   binarySearch(sort,4);
+  }
+  public static int binarySearch(int[] data, int n) {
+    int smallNum=0;
+    int highNum=data.length-1;
+    int middleNum=-1;
+    while( smallNum <= highNum ) {
+      middleNum = (smallNum+highNum)/2;
+      if (data[middleNum]>n) {
+        highNum=middleNum-1;
+      }else if(data[middleNum]<n){
+        smallNum=middleNum+1;
+      }else{
+        return middleNum;
+      }
+    }
+    System.out.println("middleNum");
+    System.out.println(middleNum);
+    return middleNum;
+  }
+}
