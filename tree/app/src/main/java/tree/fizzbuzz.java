c2c9ec37ce7090d639ee45e9a5c67edb3991ea0a
hossamhaddad <husamhaddad80@gmail.com>
2021-04-28 04:11:51 +0300

    maxvalue

--- tree/app/src/main/java/tree/fizzbuzz.java
+++ tree/app/src/main/java/tree/fizzbuzz.java
@@ -49,45 +49,4 @@ return tempNode;
 
 
 
-
-//  public FizzNode fizzBuzzTree(FizzNode oldroot){
-//    FizzNode<Node> newRoot=new FizzNode(null);
-//    Queue <FizzNode> breadth=new Queue<>();
-//    breadth.enqueue(oldroot);
-//    while (!breadth.isEmpty()){
-//      FizzNode front=breadth.dequeue();
-//      if((Integer) front.value%3==0&&(Integer) front.value%5==0){
-//        newRoot=new FizzNode("FizzBuzz");
-//        System.out.println(newRoot.value);
-//        newRoot.left=front.left;
-//        newRoot.right=front.right;
-//      }else if((Integer) front.value%3==0){
-//        newRoot=new FizzNode("Fizz");
-//        System.out.println(newRoot.value);
-//        newRoot.left=front.left;
-//        newRoot.right=front.right;
-//      }else if((Integer) front.value%5==0){
-//        newRoot=new FizzNode("Buzz");
-//        System.out.println(newRoot.value);
-//        newRoot.left=front.left;
-//        newRoot.right=front.right;
-//      }else {
-//        newRoot=new FizzNode(front.value);
-//        System.out.println(newRoot.value);
-//        newRoot.left=front.left;
-//        newRoot.right=front.right;
-//
-//      }
-//
-//      if(front.left!=null){
-//        breadth.enqueue(front.left);
-//      }
-//      if(front.right!=null){
-//        breadth.enqueue(front.right);
-//      }
-//
-//    }
-//    return newRoot;
-//  }
-
 }
